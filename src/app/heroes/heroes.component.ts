8cf79e1d6a097978020c1268069f01524d7e5f7b
Tomas Hruby <tomashrub@gmail.com>
2022-11-02 15:39:15 +0100

    1

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    if (hero === this.selectedHero) {
+      this.selectedHero = undefined;
+      this.messageService.add(`HeroesComponent: No Hero selected`);
+    } else {
+      this.selectedHero = hero;
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    }
+  }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+}
